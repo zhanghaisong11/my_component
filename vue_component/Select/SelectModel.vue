b23ae24311668c5008b2f3057a4f06bfab369c33
zhanghaisong <969382508@qq.com>
2019-08-10 14:20:12 +0800

    修改select组件，添加功能

--- vue_component/Select/SelectModel.vue
+++ vue_component/Select/SelectModel.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="select-box" v-cloak :style="domWidth">
-    <div @click="taskSelectBoxClick" >
+  <div class="select-box" v-cloak >
+    <div @click="taskSelectBoxClick">
       <p class="fl" v-text="selectLabel"></p>
-      <i class="fr el-icon-arrow-down"></i>
+      <i class="fr el-icon-caret-bottom"></i>
     </div>
-    <ul class="option-box" v-show="isShowTaskSelect" :style="domWidth">
-      <li v-for="(option,key) in options" :key="key" @click="chooseTaskType(option)" v-text="label === '' ? option : option[label]"></li>
+    <ul class="option-box" v-show="isShowTaskSelect">
+      <li v-for="(option,key) in options" :key="key" @click="chooseTaskType(option)"
+          v-text="labelKey === '' ? option : option[labelKey]"></li>
     </ul>
   </div>
 </template>
@@ -17,16 +18,16 @@
       return {
         selectLabel: '',
         isShowTaskSelect: false,
-        selectValue: '',
-        domWidth:{width:'100px'}
+        selectValue: ''
       }
     },
     model: {
-      prop: 'content',
+      prop:'content',
       event: 'contentChange'
     },
     props: {
-      content: {
+      placeholder:{
+        type: String,
         default: function () {
           return ''
         }
@@ -37,14 +38,13 @@
           return []
         }
       },
-      width: {
-        type: Number,
+      labelKey: {
+        type: String,
         default: function () {
-          return 100
+          return ''
         }
       },
-      valueKey: {
-        type: String,
+      content:{
         default: function () {
           return ''
         }
@@ -56,13 +56,35 @@
         }
       }
     },
-    created(){
-      this.domWidth.width = this.width + 'px'
-
+    created() {
+      if(this.content === ''){
+        this.selectLabel = this.placeholder
+      }else{
+        this.initSelectLabel();
+      }
     },
-    mounted(){
-    },
+    // watch:{
+    //   content:function(value){
+    //     this.initSelectLabel()
+    //   }
+    // },
     methods: {
+      initSelectLabel:function(){
+        if(this.valueKey === ''){
+          this.selectLabel = this.content
+        }else{
+          this.getChooseOption();
+        }
+      },
+      getChooseOption: function(){
+        const that = this;
+        this.options.forEach(function(option){
+          if(that.content == option[that.valueKey]){
+            that.selectLabel = option[that.labelKey]
+          }
+        })
+      },
+
       taskSelectBoxClick: function () {
         if (this.isShowTaskSelect) {
           this.isShowTaskSelect = false;
@@ -71,12 +93,16 @@
         }
       },
       chooseTaskType: function (option) {
-        if (this.label === '') {
+        if (this.labelKey === '') {
           this.selectLabel = option;
+        } else {
+          this.selectLabel = option[this.labelKey];
+        }
+        if (this.valueKey === '') {
           this.selectValue = option;
         } else {
-          this.selectLabel = option[this.label];
-          this.selectValue = option[this.value];
+          this.selectValue = option[this.valueKey];
+
         }
         this.$emit('contentChange', this.selectValue);
         this.$emit('change', this.selectValue);
@@ -96,13 +122,13 @@
   .select-box {
     height: 28px;
     line-height: 28px;
-    border: solid #e3e3e3 1px;
-    background: #f9f9f9;
-    border-radius: 3px;
+    border: solid #e0f2ff 1px;
+    background: #ffffff;
+    border-radius: 2px;
     padding-left: 20px;
-    margin: 10px;
     cursor: pointer;
     position: relative;
+    margin: 0;
   }
 
   .select-box div {
@@ -110,25 +136,41 @@
     width: 100%;
   }
 
+  .select-box div p{
+    width: calc(100% - 35px);
+    text-align: center;
+    overflow: hidden;
+    text-overflow:ellipsis;
+    white-space: nowrap;
+    height: 30px;
+  }
+
   .select-box .option-box {
     position: absolute;
-    width: 160px;
+    width: 100%;
     text-align: center;
     top: 28px;
     padding: 0;
     background: #f9f9f9;
-    left: 0px;
+    left: 0;
+    z-index: 200;
     max-height: 200px;
     overflow: auto;
+
   }
 
   .select-box .option-box li {
     font-size: 14px;
     color: #666666;
+    overflow: hidden;
+    text-overflow:ellipsis;
+    white-space: nowrap;
+    width: 100%;
+    height: 30px;
   }
 
   .select-box .option-box li:hover {
-    background: #f0f0f0;
+    background: #e0f2ff;
 
   }
 
@@ -140,6 +182,10 @@
   .select-box i {
     margin: 5px;
     color: #666666;
+  }
+
+  .el-icon-caret-bottom{
+    color: #38a8f0!important;
   }
 
 
